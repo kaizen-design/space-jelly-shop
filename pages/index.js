ab4690938f7862059afcfd824f4b7f0509bfd7f0
denis <denis@kaizen-design.ru>
2022-07-02 12:40:54 +0400

    Import error fix

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import products from '../products.json';
-import initiateCheckout from '../lib/payments.js';
+import { initiateCheckout } from '../lib/payments.js';
 
 console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 
